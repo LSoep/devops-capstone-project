f08626f53a1f2f07d8bae7a384ffb2c2862c0ac6
LSoep <leila.soep@gmail.com>
2024-08-09 19:20:33 -0400

    read an account - routes and tests

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -142,11 +142,17 @@ class TestAccountService(TestCase):
         all_accounts = response.get_json()
         self.assertEqual(len(all_accounts),0)
 
-        # With Data
-        self._create_accounts(5)
-        response = self.client.get(BASE_URL)
+    def test_read_an_account(self):
+        """It should GET an Account"""
+        account = self._create_accounts(1)[0]
+        response = self.client.get(
+            f"{BASE_URL}/{account.id}", content_type="application/json"
+        )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        all_accounts = response.get_json()
-        self.assertEqual(len(all_accounts),5)
+        account_info = response.get_json()
+        self.assertEqual(account_info["name"], account.name)
 
-
+    def test_get_account_not_found(self):
+        """It should not Read an Account that is not found"""
+        response = self.client.get(f"{BASE_URL}/0")
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
